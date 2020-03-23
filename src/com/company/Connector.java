a08c4ad9391004c9c6850276f642199ed6aa9bff
Kristian Engberg <krissoengberg@gmail.com>
2020-03-23 14:48:48 +0100

    added connector class

---
+++ src/com/company/Connector.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class Connector {
+
+    String password;
+    String username;
+    Connection con;
+
+    public Connector(String username, String password){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rfb_db",username,password);
+
+            System.out.println("Succesfully connected to server");
+
+        }
+        catch(Exception e) {
+            System.out.println("failed " + e);
+        }
+
+    }
+
+    public void closeConnection()
+    {
+        try {
+            con.close();
+            System.out.println("Closed connection successfully");
+        }
+
+        catch(SQLException e){
+            System.out.println("failed to close connection " + e);
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Connector connection = new Connector("root", "Healmelawl0210");
+
+        connection.closeConnection();
+
+
+    }
+}
