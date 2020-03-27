646b6ba09d97c09827bf5a37491195338f9c12bc
Jakob Bundgaard <jakobbundgaard@hotmail.com>
2020-03-27 14:49:09 +0100

    GUI2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,14 @@ import javax.swing.*;
 public class Main {
 
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame();
 
-        public static void main(String[] args) {
-           // SwingUtilities.invokeLater(new Runnable() {
-                //public void run() {
-                    new MainFrame();
-
-                }
-            //});
-        }
-
+            }
+        });
+    }
+}
 
 
