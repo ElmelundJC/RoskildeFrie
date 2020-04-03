9dc87ffa8603619bc068dd58f76139f51ba653df
PR31SL3R <jesperpreisler@hotmail.com>
2020-04-03 15:24:24 +0200

    id reader function added (idreader)
    id query print added

--- Database/RFB_db.sql
+++ Database/RFB_db.sql
@@ -7,6 +7,11 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema RFB_db
 -- -----------------------------------------------------
+drop database if exists `RFB_db`;
+DROP SCHEMA IF EXISTS `RFB_db` ;
+create database `RFB_db`;
+
+DROP SCHEMA IF EXISTS `RFB_db` ;
 DROP SCHEMA IF EXISTS `RFB_db` ;
 
 -- -----------------------------------------------------
@@ -66,3 +71,28 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+insert into Stue value(1,"blå");
+insert into Stue value(2,"grøn");
+insert into Stue value(3,"gul");
+
+insert into Børn value (default,"1707163465","Jens Jensen","Bakkevej",1);
+insert into Børn value (default,"1808168753","Lars Hansen","Dalsvej 2",2);
+insert into Børn value (default,"1909169875","Christian Petersen","Jordbærvej 3",3);
+insert into Børn value (default,"1606162311","Ole Poulsen","Kirsebærvej 4",1);
+insert into Børn value (default,"1505169643","Bo Frederiksen","Mandelvej 5",2);
+insert into Børn value (default,"1404162311","Victor Albertsen","Melonvej 6",3);
+insert into Børn value (default,"1303168777","Thor Larsen","Æblevej 7",1);
+insert into Børn value (default,"1202165555","Villum Thorsen","Pærevej 8",2);
+
+insert into Forældre value ("Lars Larsen",74327495,98734152,1);
+insert into Forældre value ("Kurt Thorsen",45623091,35465718,2);
+insert into Forældre value ("Hans Hansen",12659304,28374659,3);
+insert into Forældre value ("Preben Pølsetud",28374651,36409514,4);
+insert into Forældre value ("Poul Krebs",28475638,27364098,5);
+insert into Forældre value ("Lotte Ludvig",59348712,28374651,6);
+insert into Forældre value ("Lis Poulsen",26348576,23619999,7);
+insert into Forældre value ("Beate Jensen",16253475,182736451,8);
+
+
+
